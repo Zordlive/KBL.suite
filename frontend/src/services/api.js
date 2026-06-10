5f15ee8f66c849c121eba2a7aeadd1b7101c00a7
Martin <martin.lub999@gmail.com>
2026-06-10 13:51:24 +0100

    à 95%

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // Adjust to your backend URL
+  baseURL: 'https://kilubak.shop/api', // Adjust to your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
